77fa18113c7b021af63252fcc0fbbd1e9ce2b335
Carley Tripp <carley.tripp@outlook.com>
2019-04-04 21:17:45 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root 
+  root 'login'
 
   get 'secrets/show'
 
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
   post 'logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
